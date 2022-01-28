5ba7a1612d0b2b8e338e3d839841e4cbe1a1a435
dravid3x <39882540+dravid3x@users.noreply.github.com>
2022-01-28 12:20:39 +0100

    BEEANS v2

--- src/main/java/beans/CorsaBean.java
+++ src/main/java/beans/CorsaBean.java
@@ -1,5 +1,76 @@
 package beans;
+import java.time.LocalDate;
 
-public class CorsaBean {
 
+public class CorsaBean implements java.io.Serializable{
+	private int IDCorsa;
+	private int IDFDipedente;
+	private int IDFTukTuk;
+	private LocalDate DataInizio;
+	private LocalDate DataFine;
+	private float Chilometri;
+	private float TariffaCorsa;
+	
+	public CorsaBean() {
+		this.Chilometri = 0;
+		this.TariffaCorsa = 0;
+	}
+
+	public int getIDCorsa() {
+		return IDCorsa;
+	}
+
+	public void setIDCorsa(int iDCorsa) {
+		IDCorsa = iDCorsa;
+	}
+
+	public int getIDFDipedente() {
+		return IDFDipedente;
+	}
+
+	public void setIDFDipedente(int iDFDipedente) {
+		IDFDipedente = iDFDipedente;
+	}
+
+	public int getIDFTukTuk() {
+		return IDFTukTuk;
+	}
+
+	public void setIDFTukTuk(int iDFTukTuk) {
+		IDFTukTuk = iDFTukTuk;
+	}
+
+	public LocalDate getDataInizio() {
+		return DataInizio;
+	}
+
+	public void setDataInizio(LocalDate dataInizio) {
+		DataInizio = dataInizio;
+	}
+
+	public LocalDate getDataFine() {
+		return DataFine;
+	}
+
+	public void setDataFine(LocalDate dataFine) {
+		DataFine = dataFine;
+	}
+
+	public float getChilometri() {
+		return Chilometri;
+	}
+
+	public void setChilometri(float chilometri) {
+		Chilometri = chilometri;
+	}
+
+	public float getTariffaCorsa() {
+		return TariffaCorsa;
+	}
+
+	public void setTariffaCorsa(float tariffaCorsa) {
+		TariffaCorsa = tariffaCorsa;
+	}
+	
+	
 }
