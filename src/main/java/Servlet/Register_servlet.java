5de905a214e8947b6f4c7cbdd3a7bfa8aa77e195
Paulpetta668 <48731892+Paulpetta668@users.noreply.github.com>
2022-02-22 10:34:23 +0100

    Update Register_servlet.java

--- src/main/java/Servlet/Register_servlet.java
+++ src/main/java/Servlet/Register_servlet.java
@@ -20,18 +20,12 @@ public class Register_servlet extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		ConnessioneDB c = new ConnessioneDB();
 		
