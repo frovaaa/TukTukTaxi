747b780245cc61cdaf3fa188b47bdcc7a843a603
dravid3x <39882540+dravid3x@users.noreply.github.com>
2022-04-25 17:55:50 +0200

    Aaaa

--- src/main/java/Servlet/aggiuntaCorsa_servlet.java
+++ src/main/java/Servlet/aggiuntaCorsa_servlet.java
@@ -35,7 +35,7 @@ public class aggiuntaCorsa_servlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+
 	}
 
 }
